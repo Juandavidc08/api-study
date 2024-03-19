64a9dedcec0d24e46e928f3408a2869a8bc1de55
Juandavid08 <jdavidcamelo94@gmail.com>
2024-03-19 16:04:54 +0100

    Constants created in Javascript

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const API_KEY ="Un5ZnuxiqAdH1AYWCyCS8MdNHto";
+const API_URL= "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultModal"))
